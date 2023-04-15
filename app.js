3456e0a41a5bfd1787683ba72ac03fc74b6c551e
N French :) <72806068+NaNNaNNaNNaNBoolean@users.noreply.github.com>
2023-04-15 18:47:43 +0100

    Just need to fix the try in PATCH

--- app.js
+++ app.js
@@ -1,102 +1,89 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
 
-const app = express()
+const app = express();
+
+const {capitalise} = require('./capitalise')
+//made my own
+const mangas = require('./mangas.json');
 
 app.use(cors())
 app.use(express.json())
-//made my ownnnnn
-const mangas = require("./mangas.json")
 
-app.get('/', (req, res) => {
-    res.send('Hello to the very small manga !')
-  })
-  
-  app.get('/mangas', (req,res) => {
-    
-      res.send("IT READING TIME!")
-      //res.send(mangas)
-  })
-  
-//   app.get('/mangas/:id', (req,res) => {
-//     const id = req.params.id;
-//     const manga = mangas.find(book => book.id === id);
-//       //res.send(req.params)
-//       console.log(manga) //undefined
-//       //res.send(manga)
-//   })
+app.get('/',(req,res) => {
+    res.send("Hello, welcome to the very small manga collection!")
+})
 
-/////////////////////////////////////////////////////////////////
+app.get('/mangas',(req,res) => {
+    res.send(mangas)
+})
+
+//////////////////////////////////////////////////////////////////////////
 //get manga info from json
-  app.get('/mangas/:id', (req,res) => {
-      //console.log(req.params.title)
-      //const id = req.params.id; //for testing
-      const manga = mangas.find(manga => manga.id === req.params.id); //if manga id matchs web id
-      console.log(manga) //undefineddddddddd but whyyyyyyyyy
-      if(manga === undefined){
-        //const title = req.params.title
-        const maName = mangas.findIndex(i => i.title === manga)
-          res.status(404).send({error: `Manga ${maName} is not found`})
-      }
-      res.send(manga)
-  })
 
-  ////////////////////////////////////////////////////////////////////////////
-  ////All needs further testing/////////
+app.get('/mangas/:id', (req,res) => {
+    const id = req.params.id;
+    const manga = mangas.find(manga => manga.id == id);
+    console.log(manga)
+    if(manga === undefined){
+        //const maName = mangas.findIndex(i => i.title === manga)
+        res.status(404).send({ error: `Manga with identifaction ${id} is not found`})
+    }
+    res.send(manga)
+})
+
+////////////////////////////////////////////////////////////////////////////
+
+app.post('/mangas', (req, res) => {
+    const ids = mangas.map(manga => manga.id)
+    let maxId = Math.max(...ids)
+    const manga = mangas.find(manga => manga.id == req.body.id)
   
-  app.post('/mangas', (req,res) => {
-      const ids = mangas.map((manga) => manga.id);
-      let maxId = Math.max(...ids);
-      //... to work like an array
-      const manga = mangas.find(manga => manga.title === req.body.title)
-      if (manga !== undefined) {
-          res.status(409).send({error: "Manga already exists"})
-      }else{
-          maxId +=1
-          const newManga = req.body
-          newManga.id = maxId
-  
-          mangas.push(newManga) //new manga who this?
-  
-          res.status(201).send(newManga)
-  
-      }
-      res.status(201).send(newManga);
+    if (manga !== undefined) {
+      res.status(409).send({error: "manga already exists"})
+    } else {
+      maxId += 1
+      const newManga = req.body
+      newManga.id = maxId
+      mangas.push(newManga)
+      res.status(201).send(newManga)
+    }
   })
 
   ///////////////////////////////////////////////////////////////////////////
-  
+
   app.patch("/mangas/:id", (req, res) => {
-      const manga = mangas.find(manga => manga.id === req.params.id);
-    
-      if (manga === undefined) {
-        return res.status(404).send({error: "manga does not exist"})
-      }
-    
-      try {
-        const updatedManga = { ...req.body, id:req.body.title, title: manga.title}
+    const manga = mangas.find(manga => manga.id == req.params.id);
+   
+    if (manga === undefined) {
+      return res.status(404).send({error: "Manga does not exist"})
+    }
+  //////////////////////////////////////////////////////////////////////////////////
+    try {//change needed // not woring somhow for manga to id
+    const updatedManga = { ...req.body, name: capitalise(req.body.name), id: manga.id};
+      const idx = mangas.findIndex(m => m.id == manga.id);
+      mangas[idx] = updatedManga;
+      res.send(updatedManga)
+    } catch (error) {
+      res.status(400).send(error.message)
 
-        const maName = mangas.findIndex(i => i.title === manga.title);
-        mangas[maName] = updatedManga;
-        res.send(updatedManga)
-      } catch (error) {
-        res.status(400).send(error.message)
-      }
-    })
+    }
+  })
 
-  ///////////////////////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////////////////////////
 
-    app.delete("/mangas/:id", (req,res)=> {
-      //const ID = req.params.id; // get the id?
-      const mangaIx = mangas.findIndex(manga => manga.id === req.params.id);
+  app.delete("/mangas/:id", (req, res) => {
+    const id = req.params.id;
+    const manga = mangas.find(manga => manga.id == id);
   
-      if(mangaIx === -1){
-          res.status(404).send({error: "Manga does not exist"})
-      }else{
-          mangas.splice(mangaIx, 1);
-          res.status(204).send() //goodbye
-      }
-    })
+    if (manga === -1) {
+      res.status(404).send({ error: "manga does not exist" })
+    } else {
+      mangas.splice(manga, 1);
   
-  module.exports = app;
-  
+      res.status(204).send()
+    }
+  })
+
+
+module.exports =app;
